08b1f26e54058acf912580942ce01cde75cc5800
chenhaotian <chenhaotian@kylinos.com.cn>
2022-02-21 17:30:52 +0800

    backend: reorganize server code
    - server: reorganize server code, tls for controller service
    - config: remove flags
    - config: read toml config file
    - systemd: update command line

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"flag"
-	"fmt"
+	"log"
 	"net"
 	"os"
-	"path/filepath"
-	"time"
 
-	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/keepalive"
 
 	"ksc-mcube/common"
 	"ksc-mcube/server/agent"
@@ -22,70 +17,53 @@ const (
 	controllerMode = "controller"
 )
 
-var (
-	logDir    string
-	logStdout bool
-	verbose   int
-	mode      string
-)
-
 func main() {
-	flag.Parse()
-
-	if flag.NArg() > 0 {
-		mode = flag.Arg(0)
+	if len(os.Args) != 3 {
+		usage()
 	}
 
-	if flag.NArg() != 1 || (mode != agentMode && mode != controllerMode) {
-		fmt.Fprintf(flag.CommandLine.Output(), "%s need one arg, value must be '%s' or '%s':\n", os.Args[0], agentMode, controllerMode)
-		flag.Usage()
-		os.Exit(1)
+	mode := os.Args[1]
+	configFile := os.Args[2]
+	if mode != agentMode && mode != controllerMode {
+		usage()
+	}
+
+	if err := common.LoadConfig(configFile); err != nil {
+		log.Fatalf("load config file=%s error=%v", configFile, err)
 	}
 
 	// TODO check log dir, create when it not exists.
-	logFile := filepath.Join(logDir, mode)
-	common.InitLogger(verbose, logStdout, logFile)
+	common.InitLogger(mode)
 
-	var addr string
-	opts := append(common.UnaryServerInterceptor(),
-		grpc.KeepaliveEnforcementPolicy(keepalive.EnforcementPolicy{
-			MinTime:             10 * time.Second, // If a client pings more than once every 10 seconds, terminate the connection
-			PermitWithoutStream: true,             // Allow pings even when there are no active streams
-		}),
-		grpc.KeepaliveParams(keepalive.ServerParameters{
-			MaxConnectionIdle:     60 * time.Second, // If a client is idle for 60 seconds, send a GOAWAY
-			MaxConnectionAge:      30 * time.Minute, // If any connection is alive for more than 30 minutes, send a GOAWAY
-			MaxConnectionAgeGrace: 5 * time.Second,  // Allow 5 seconds for pending RPCs to complete before forcibly closing connections
-			Time:                  20 * time.Second, // Ping the client if it is idle for 20 seconds to ensure the connection is still active
-			Timeout:               5 * time.Second,  // Wait 5 second for the ping ack before assuming the connection is dead
-		}))
-	s := grpc.NewServer(opts...)
+	var (
+		addr   string
+		err    error
+		server *grpc.Server
+	)
+
 	if mode == agentMode {
-		agent.Register(s)
-		addr = fmt.Sprintf("%s:%d", common.Host, common.AgentPort)
+		server, err = agent.Server()
+		addr = common.Config.Agent.Addr()
 	} else {
-		controller.Register(s)
-		addr = fmt.Sprintf("%s:%d", common.Host, common.ControllerPort)
+		server, err = controller.Server()
+		addr = common.Config.Controller.Addr()
+	}
+
+	if err != nil {
+		log.Fatalf("init service, error=%v", err)
 	}
 
 	lis, err := net.Listen("tcp", addr)
 	if err != nil {
-		logrus.Fatalf("failed to listen(%s): %v", addr, err)
+		log.Fatalf("failed to listen addr=%s error=%v", addr, err)
 	}
 
-	logrus.Printf("%s server listening at %v", mode, lis.Addr())
-	if err := s.Serve(lis); err != nil {
-		logrus.Fatalf("failed to serve: %v", err)
+	// log.Printf("%s server listening at %v", mode, lis.Addr())
+	if err := server.Serve(lis); err != nil {
+		log.Fatalf("failed to serve, error=%v", err)
 	}
 }
 
-func init() {
-	flag.StringVar(&logDir, "logdir", "/var/log/ksc-mcube/", "log output directory")
-	flag.BoolVar(&logStdout, "stdout", false, "log write to stdout")
-	flag.IntVar(&verbose, "verbose", int(logrus.InfoLevel), "log verbosity: trace=6 debug=5 info=4 warning=3 error=2")
-
-	flag.Usage = func() {
-		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s [OPTIONS] agent|controller:\n", os.Args[0])
-		flag.PrintDefaults()
-	}
+func usage() {
+	log.Fatalf("Usage: %s controller|agent CONFIG_FILE", os.Args[0])
 }

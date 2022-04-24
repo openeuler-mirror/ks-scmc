393f5ad60f555c46a11584ed26204d41831a4c26
chenhaotian <chenhaotian@kylinos.com.cn>
2022-04-24 15:26:51 +0800

    feature(server): authz plugin service
    - Docker 插件阻止敏感容器通过命令行管理
    - RPC 服务提供增删敏感容器管理接口
    - Docker API 客户端加入authz认证头部信息

---
+++ backend/cmd/ks-scmc-authz/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os/user"
+	"strconv"
+
+	"github.com/docker/go-plugins-helpers/authorization"
+)
+
+var (
+	pluginSocket = "/run/docker/plugins/authz-plugin.sock"
+	rpcSocket    = "/var/lib/ks-scmc/authz.sock"
+	configPath   = "/etc/ks-scmc/authz.json"
+)
+
+func main() {
+	flag.Parse()
+
+	if err := initConfig(configPath); err != nil {
+		log.Printf("initConfig: %v", err)
+	}
+
+	if err := initRPCServer(rpcSocket); err != nil {
+		log.Printf("initRPCServer: %v", err)
+	}
+
+	u, _ := user.Lookup("root")
+	gid, _ := strconv.Atoi(u.Gid)
+	handler := authorization.NewHandler(&AuthZPlugin{magicUser: "KS-SCMC-SERVICE"})
+	if err := handler.ServeUnix(pluginSocket, gid); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func init() {
+	flag.StringVar(&pluginSocket, "plugin-sock", "/run/docker/plugins/authz-plugin.sock", "Plugin serve socket, under dir /run/docker/plugins/")
+	flag.StringVar(&rpcSocket, "rpc-sock", "/var/lib/ks-scmc/authz.sock", "RPC socket to receive setting changes")
+	flag.StringVar(&configPath, "config-path", "/etc/ks-scmc/authz.json", "Config file path")
+}

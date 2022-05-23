3fa4aec1b92e2112a186fb602550d44042e5cd54
chenhaotian <chenhaotian@kylinos.com.cn>
2022-05-23 16:38:51 +0800

    fix(cmd): update ks-scmc-user and related scripts
    - ks-scmc-user 增加verbose选项
    - init_users.sh 添加更多提示语, 优化创建管理员用户
    - sysadm 容器常规配置权限
    - secadm 容器安全配置，镜像查看审批权限
    - Makefile 安装二进制文件与相关脚本
    
    Fix #55825

--- backend/cmd/ks-scmc-user/main.go
+++ backend/cmd/ks-scmc-user/main.go
@@ -15,6 +15,12 @@ import (
 	pb "scmc/rpc/pb/user"
 )
 
+var (
+	flagServerAddr = flag.String("s", "127.0.0.1:10050", "server address")
+	flagCmd        = flag.String("c", "", "user command(list|create|update|remove + _ + user|role)")
+	flagVerbose    = flag.Bool("v", false, "verbose mode to print requests and replies")
+)
+
 func makeCall(f func(context.Context)) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	ctx = metadata.AppendToOutgoingContext(ctx, "authorization", "0:xxxxx")
@@ -30,10 +36,12 @@ func parseRequest(args string, req interface{}) {
 }
 
 func printReply(reply interface{}) {
-	if s, err := json.MarshalIndent(reply, "", "  "); err != nil {
-		log.Printf("reply: %+v", reply)
-	} else {
-		log.Printf("reply: %s", s)
+	if *flagVerbose {
+		if s, err := json.MarshalIndent(reply, "", "  "); err != nil {
+			log.Printf("reply: %+v", reply)
+		} else {
+			log.Printf("reply: %s", s)
+		}
 	}
 }
 
@@ -140,6 +148,56 @@ func removeRole(cli pb.UserClient, args string) {
 	})
 }
 
+func createUserWithRole(cli pb.UserClient, username, role string) {
+	var roleID int64
+	makeCall(func(ctx context.Context) {
+		rep, err := cli.ListRole(ctx, &pb.ListRoleRequest{})
+		if err != nil {
+			log.Fatalf("查询角色列表错误: %v", err)
+		}
+		for _, r := range rep.Roles {
+			if r.Name == role {
+				roleID = r.Id
+				return
+			}
+		}
+		log.Fatalf("角色%s不存在", role)
+	})
+
+	makeCall(func(ctx context.Context) {
+		_, err := cli.CreateUser(ctx, &pb.CreateUserRequest{
+			UserInfo: &pb.UserInfo{
+				LoginName:  username,
+				Password:   "12345678",
+				IsActive:   true,
+				IsEditable: false,
+				RoleId:     roleID,
+			},
+		})
+		if err != nil {
+			log.Fatalf("创建用户%s 角色%s 错误: %v", username, role, err)
+		}
+	})
+}
+
+func createAdmin(cli pb.UserClient, args string) {
+	for _, u := range []string{"sysadm", "secadm", "audadm"} {
+		createUserWithRole(cli, u, u+"_r")
+	}
+}
+
+func createSysadm(cli pb.UserClient, username string) {
+	createUserWithRole(cli, username, "sysadm_r")
+}
+
+func createSecadm(cli pb.UserClient, username string) {
+	createUserWithRole(cli, username, "secadm_r")
+}
+
+func createAudadm(cli pb.UserClient, username string) {
+	createUserWithRole(cli, username, "audadm_r")
+}
+
 func handleInput(serverAddr, cmd, args string) {
 	var f func(pb.UserClient, string)
 	switch strings.ToLower(cmd) {
@@ -159,6 +217,14 @@ func handleInput(serverAddr, cmd, args string) {
 		f = updateRole
 	case "remove_role":
 		f = removeRole
+	case "create_admin":
+		f = createAdmin
+	case "create_sysadm":
+		f = createSysadm
+	case "create_secadm":
+		f = createSecadm
+	case "create_audadm":
+		f = createAudadm
 	default:
 		log.Fatalf("unknown cmd[%s]", cmd)
 	}
@@ -176,8 +242,6 @@ func handleInput(serverAddr, cmd, args string) {
 }
 
 func main() {
-	serverAddr := flag.String("s", "127.0.0.1:10050", "server address")
-	cmd := flag.String("c", "", "user command(list|create|update|remove + _ + user|role)")
 	flag.Parse()
 
 	argsLen := len(flag.Args())
@@ -185,7 +249,9 @@ func main() {
 		log.Fatal("need arguments")
 	}
 	args := flag.Args()[0]
-	log.Print("args: ", args)
+	if *flagVerbose {
+		log.Print("args: ", args)
+	}
 
-	handleInput(*serverAddr, *cmd, args)
+	handleInput(*flagServerAddr, *flagCmd, args)
 }
